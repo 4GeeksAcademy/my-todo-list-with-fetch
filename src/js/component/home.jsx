e762891a93db1aeec65641cb9bf830424f9a79f6
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-15 21:31:50 +0000

    asadsadsad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,110 +1,140 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 function Home() {
+  const [toDoArr, setToDoArr] = useState(["Do work"]);
+  const [input, setInput] = useState("");
+  const [usersList, setUsersList] = useState([]);
+  const [userDeleted, setUserDeleted] = useState(true);
+  const [tasks, setTasks] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
 
-  const [toDoArr, setToDoArr] = useState(["Do work"])
-  const [input, setInput] = useState("")
-  const [usersList, setUsersList] = useState([])
-  const [userDeleted, setUseDeleted] = useState(true)
-  const [getTask, setGetTask ] = useEffect ([])
+  useEffect(() => {
+    getUsers();
+  }, [userDeleted]);
 
-  function addItem() {
-    let result = input.trim()
-    if (!result) {
-      alert("You need to enter valid text!")
-    } else {
-      setToDoArr(toDoArr.concat([result]))
-      setInput("")
-    }
+  function getUsers() {
+    fetch("https://playground.4geeks.com/todo/users")
+      .then((response) => response.json())
+      .then((data) => setUsersList(data.users));
+  }
 
-  };
-
-  const listItems = toDoArr.map((item, index) =>
-    <li className="d-flex justify-content-between hoverParent">
-      <p className="m-2" >{item}</p>
-      <button className="btn btn-danger hoverButton"
-        onClick={() => setToDoArr(toDoArr.filter((x, i) => { return index != i }))}
-      ><i class="fa-solid fa-x"></i></button>
-    </li>
-  )
-
-  function CreateUser() {
-    fetch('https://playground.4geeks.com/todo/', {
-      Method: "POST"
+  function CreateUser(username) {
+    fetch(`https://playground.4geeks.com/todo/users/${username}`, {
+      method: "POST",
     })
       .then((response) => response.json())
       .then((data) => {
         if (data.id) {
-          alert("Usuario creado con éxito")
-          setUserDeleted(prev => !prev)
-  
+          alert("Usuario creado con éxito");
+          setUserDeleted((prev) => !prev);
         } else {
-          alert("Algo salió mal")
+          alert("Algo salió mal");
         }
       })
-  
-  
-      .catch((error) => console.log(error))
-  
-  };
-  
-  function deleteUser() {
-    fetch('https://playground.4geeks.com/todo/', {
-      method: "DELETE"
+      .catch((error) => console.log(error));
+  }
+
+  function deleteUser(username) {
+    fetch(`https://playground.4geeks.com/todo/${username}`, {
+      method: "DELETE",
     })
       .then((data) => {
         if (data.ok) {
-          alert("Usuario eliminado con éxito")
-          setUserDeleted(prev => !prev)
+          alert("Usuario eliminado con éxito");
+          setUserDeleted((prev) => !prev);
         } else {
-          alert ("Algo salió mal")}
-      })
-    };
-  
-  const getUsers = () => {
-    fetch('https://playground.4geeks.com/todo/users')
-      .then((response) => response.json())
-        .then((data) =>
-          setUsersList(data.users))
-    }
-  
-  
-    useEffect(() => {
-      getUsers()
-    }, [userDeleted])
-  
-    const getList = () => {
-      fetch('https://playground.4geeks.com/todo/users') 
-      .then((response)=>response.json())
-      .then((data)=> {console.log(data)})
-  
-    }
-  return (
-    <div className="mx-auto" style={{widht:"35vw"}}>
-      <input type="text" onChange={(e) => setName (e.target.value)} />
-      <button onClick={CreateUser}  ></button>
-    <div className="container">
-      <h1 className="text-center">To Do List</h1>
-      <ul>
-        <li className="d-flex">
-          <input
-            onChange={e => setInput(e.target.value)}
-            onKeyDown={e => {
-              if (e.key === "Enter") {
-                addItem()
-              }
-            }}
-            value={input} placeholder="What needs to be done?" />
-          <button className="btn btn-primary" onClick={addItem}>Submit</button>
-        </li>
-        {listItems.length === 0 ? <li><p className="m-2" >No tasks, add a task.</p></li> : ""}
-        {listItems}
-      </ul>
-      <p>{listItems.length} item{listItems.length != 1 ? "s" : ""} left</p>
+          alert("Algo salió mal");
+        }
+      });
+  }
 
+  function getTasks(username) {
+    fetch(`https://playground.4geeks.com/todo/users/${username}`)
+      .then((response) => response.json())
+      .then((data) => setTasks(data.todos));
+  }
+
+  function addItem() {
+    let result = input.trim();
+    if (!result) {
+      alert("You need to enter valid text!");
+    } else {
+      setToDoArr(toDoArr.concat([result]));
+      setInput("");
+    }
+  }
+
+  const listItems = toDoArr.map((item, index) => (
+    <li className="d-flex justify-content-between hoverParent">
+      <p className="m-2">{item}</p>
+      <button
+        className="btn btn-danger hoverButton"
+        onClick={() => setToDoArr(toDoArr.filter((x, i) => i !== index))}
+      >
+        <i class="fa-solid fa-x"></i>
+      </button>
+    </li>
+  ));
+
+  return (
+    <div className="mx-auto" style={{ width: "35vw" }}>
+      <input
+        type="text"
+        value={selectedUser}
+        onChange={(e) => setSelectedUser(e.target.value)}
+        placeholder="Select a user"
+      />
+      <button onClick={() => getTasks(selectedUser)}>Get tasks</button>
+      <button onClick={CreateUser}>Crear usuario</button>
+      <button onClick={deleteUser}>Eliminar usuario</button>
+
+      <label htmlFor="">Lista de usuarios</label>
+      {usersList.map((item, index) => (
+        <h5 key={index}>{item.name}</h5>
+      ))}
+
+      <div className="container">
+        <h1 className="text-center">To Do List</h1>
+
+        <ul>
+          <li className="d-flex">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  addItem();
+                }
+              }}
+              value={input}
+              placeholder="What needs to be done?"
+            />
+            <button className="btn btn-primary" onClick={addItem}>
+              Submit
+            </button>
+          </li>
+
+          {listItems.length === 0 ? (
+            <li>
+              <p className="m-2">No tasks, add a task.</p>
+            </li>
+          ) : (
+            ""
+          )}
+          {listItems}
+        </ul>
+        <p>
+          {listItems.length} item{listItems.length !== 1 ? "s" : ""}
+        </p>
+
+        <h2>Tasks for {selectedUser}</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
